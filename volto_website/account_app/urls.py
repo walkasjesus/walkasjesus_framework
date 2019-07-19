5961ac34cace32b351e9fb86af057ad4c539df43
Lambert <verzend.aan@gmail.com>
2019-07-19 17:37:29 +0200

    Added account index page and added change password

--- volto_website/account_app/urls.py
+++ volto_website/account_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
-    path('profile', views.profile, name='profile')
+    path('profile', views.profile, name='profile'),
+    path('change_password', views.change_password, name='change_password')
 ]
--- volto_website/account_app/views.py
+++ volto_website/account_app/views.py
@@ -1,10 +1,14 @@
 from django.contrib import messages
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.shortcuts import render, redirect
 
 from account_app.forms import UserForm
+
+
+def index(request):
+    return render(request, 'account/index.html')
 
 
 def signup(request):
@@ -36,4 +40,22 @@ def profile(request):
         user_form = UserForm(instance=request.user)
     return render(request, 'account/profile.html', {
         'user_form': user_form
+    })
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'account/change_password.html', {
+        'form': form
     })

89d831117442b9b082acd85ddadc7a2b6207edb0
Lambert <verzend.aan@gmail.com>
2019-07-19 20:14:23 +0200

    Fix create user script, for some reason the indenting on for loops does not work as expected running the django shell

--- volto_website/tools/create_users.py
+++ volto_website/tools/create_users.py
@@ -2,5 +2,4 @@ from django.contrib.auth.models import User
 
 admins = [('admin', 'admin', 'admin@email.com')]
 
-for name, password, email in admins:
-    User.objects.create_superuser(name, password, email)
+[User.objects.create_superuser(name, password, email) for name, password, email in admins]

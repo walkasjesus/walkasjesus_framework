67081ccc238cbf1f7241d4bd07604f20b71e00dc
Lambert <verzend.aan@gmail.com>
2019-07-19 08:59:55 +0200

    Included auth module

--- volto_website/volto_website/urls.py
+++ volto_website/volto_website/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('callings/', include('callings_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]

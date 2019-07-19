711b1a7acd8edac392be2ff63da042c7215c4369
Lambert <verzend.aan@gmail.com>
2019-07-19 13:21:13 +0200

    Added profile page.

--- volto_website/account_app/urls.py
+++ volto_website/account_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('profile', views.profile, name='profile')
 ]
--- volto_website/account_app/views.py
+++ volto_website/account_app/views.py
@@ -1,6 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from account_app.forms import UserForm
 
 
 def signup(request):
@@ -16,3 +20,20 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        user_form = UserForm(instance=request.user)
+    return render(request, 'account/profile.html', {
+        'user_form': user_form
+    })

a3471c135fcee60aa849aecfacdb648bed082ab7
Lambert <verzend.aan@gmail.com>
2019-07-26 13:53:59 +0200

    Updated unit test, now verse uses another api call

--- bible_lib/bible_lib/tests/test_bible.py
+++ bible_lib/bible_lib/tests/test_bible.py
@@ -14,13 +14,11 @@ class TestBible(TestCase):
 
     def test_get_verse(self):
         bible = Bible(bible_id='ead7b4cc5007389c-01')
-        bible.client.get = Mock(return_value=DummyResponses().verses())
-        verse = bible.verse(BibleBooks.Daniel, 2, 4)
+        bible.client.get = Mock(return_value=DummyResponses().search_verses())
+        verse = bible.verse(BibleBooks.John, 1, 51)
 
-        bible.client.get.assert_called_with('bibles/ead7b4cc5007389c-01/verses/DAG.2.4')
-        self.assertIn('De magiërs gaven den koning ten antwoord: De koning leve voor eeuwig! Verhaal de droom aan uw '
-                      'dienaars, dan zullen wij er de uitleg van geven.',
-                      verse)
+        bible.client.get.assert_called_with('bibles/ead7b4cc5007389c-01/search?query=JHN.1.51-1.51')
+        self.assertIn('En Hij sprak tot hem: Voorwaar, voorwaar, Ik zeg u:', verse)
 
     def test_get_verses_spanning_multiple_chapters(self):
         bible = Bible(bible_id='ead7b4cc5007389c-01')

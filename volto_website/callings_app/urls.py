504c7fac3ed4440e1e580d3466c2d458e7684b0f
Lambert <verzend.aan@gmail.com>
2019-07-27 13:42:43 +0200

    Started integrating a bootstrap website with real content.

--- volto_website/callings_app/urls.py
+++ volto_website/callings_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from callings_app.views.demo_view import DemoView
 from callings_app.views.detail_view import DetailView
 from callings_app.views.index_view import IndexView
 from callings_app.views.listing_view import ListingView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('demo', DemoView.as_view(), name='demo'),
     path('listing', ListingView.as_view(), name='listing'),
     path('detail/<int:calling_id>/', DetailView.as_view(), name='detail'),
 ]

cf7df76b301b696088c1df1148a735648741c6e9
Lambert <verzend.aan@gmail.com>
2019-07-27 16:13:55 +0200

    Added a bootstrap style messages.

--- volto_website/volto_website/settings.py
+++ volto_website/volto_website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/callings/'
 LOGOUT_REDIRECT_URL = '/callings/'
+
+
+# Map django message level to Bootstrap alert, using the tag attribute
+# Now message.level is the django name, message.tag is the bootstrap name.
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}

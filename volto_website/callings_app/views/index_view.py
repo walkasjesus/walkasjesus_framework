8495ea1dd95859cac5548167d8d29f681ea91892
Lambert <verzend.aan@gmail.com>
2019-07-27 16:43:23 +0200

    Added template for single calling making it reusable

--- volto_website/callings_app/views/index_view.py
+++ volto_website/callings_app/views/index_view.py
@@ -6,5 +6,5 @@ from callings_app.models import Calling
 
 class IndexView(View):
     def get(self, request):
-        calling_list = Calling.objects.order_by('quote')[0:20]
-        return render(request, 'callings/index.html', {'calling_list': calling_list})
+        callings = Calling.objects.order_by('quote')[0:20]
+        return render(request, 'callings/index.html', {'callings': callings})

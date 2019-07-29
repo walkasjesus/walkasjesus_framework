1ce747c0ce9f6d3c2959bd52cef2aad2d8e42f75
Lambert <verzend.aan@gmail.com>
2019-07-29 08:30:34 +0200

    Added parsing chapters and verses, but not yet books or multiple chapters like Ps 1:2-2:3

--- import_tool/import_tool/bible_reference.py
+++ import_tool/import_tool/bible_reference.py
@@ -1,3 +1,5 @@
+import re
+
 from bible_lib.bible_books import BibleBooks
 
 
@@ -12,5 +14,19 @@ class BibleReference:
     @staticmethod
     def create_from_string(verse_string):
         reference = BibleReference()
+
+        pattern = r'(.*) (\d+):(\d+)-?(\d+)?'
+        # translate things like 1 Joh 2:1-3
+        match = re.match(pattern, verse_string)
+        groups = match.groups()
+
+        if len(groups) > 3:
+            reference.start_chapter = groups[1]
+            reference.start_verse = groups[2]
+            reference.end_chapter = reference.start_chapter
+        if groups[3] is None:
+            reference.end_verse = reference.start_verse
+        else:
+            reference.end_verse = groups[3]
 
         return reference
--- import_tool/import_tool/tests/test_bible_reference.py
+++ import_tool/import_tool/tests/test_bible_reference.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, skip
 
 from bible_lib import BibleBooks
 
@@ -6,10 +6,39 @@ from import_tool.bible_reference import BibleReference
 
 
 class TestBibleReference(TestCase):
-    def test_create_from_string(self):
-        ref = BibleReference().create_from_string('Ps 42:2-3')
+    def test_create_from_string_span_verse(self):
+        ref = BibleReference().create_from_string('Ps 42:12-13')
+
         self.assertEqual(BibleBooks.Psalms, ref.book)
         self.assertEqual(42, ref.start_chapter)
-        self.assertEqual(2, ref.start_verse)
+        self.assertEqual(12, ref.start_verse)
         self.assertEqual(42, ref.end_chapter)
+        self.assertEqual(13, ref.end_verse)
+
+    def test_create_from_string_book_with_number(self):
+        ref = BibleReference().create_from_string('1 Joh 2:3')
+
+        self.assertEqual(BibleBooks.JohnFirstBook, ref.book)
+        self.assertEqual(2, ref.start_chapter)
+        self.assertEqual(3, ref.start_verse)
+        self.assertEqual(2, ref.end_chapter)
         self.assertEqual(3, ref.end_verse)
+
+    def test_create_from_string_simple(self):
+        ref = BibleReference().create_from_string('Gen 1:2')
+
+        self.assertEqual(BibleBooks.Genesis, ref.book)
+        self.assertEqual(1, ref.start_chapter)
+        self.assertEqual(2, ref.start_verse)
+        self.assertEqual(1, ref.end_chapter)
+        self.assertEqual(2, ref.end_verse)
+
+    @skip('Valid case, but not implemented yet')
+    def test_create_from_string_multiple_chapters(self):
+        ref = BibleReference().create_from_string('2 Jon 3:4-5:6')
+
+        self.assertEqual(BibleBooks.JohnSecondBook, ref.book)
+        self.assertEqual(3, ref.start_chapter)
+        self.assertEqual(4, ref.start_verse)
+        self.assertEqual(5, ref.end_chapter)
+        self.assertEqual(6, ref.end_verse)

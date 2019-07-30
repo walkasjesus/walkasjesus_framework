82dd238639c934de18b773a6fb2848fc42df5520
Lambert <verzend.aan@gmail.com>
2019-07-30 13:31:41 +0200

    fixed a few bugs

--- volto_website/commandments_app/views/index_view.py
+++ volto_website/commandments_app/views/index_view.py
@@ -6,5 +6,5 @@ from commandments_app.models import Commandment
 
 class IndexView(View):
     def get(self, request):
-        commandments = Commandment.objects.order_by('quote')[0:20]
+        commandments = Commandment.objects.order_by('title')[0:20]
         return render(request, 'commandments/index.html', {'commandments': commandments})

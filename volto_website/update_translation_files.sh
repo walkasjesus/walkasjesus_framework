3734a1eefc5d8eb2d761fd1a66ce8b1582f7d844
Lambert <verzend.aan@gmail.com>
2019-08-02 13:21:00 +0200

    Found out why translation did not work, added compile command. After editing .po files run the compile command (update_translation_files.sh)

--- volto_website/update_translation_files.sh
+++ volto_website/update_translation_files.sh
@@ -5,4 +5,7 @@ elif [[ -f ./venv/bin/activate ]]; then
 else
 	echo "error: cannot find environment binary"
 fi
+echo "Adding new texts to the .po files."
 django-admin makemessages -l nl
+echo "Compiling the .po files. Run this also after changing the .po files."
+django-admin compilemessages

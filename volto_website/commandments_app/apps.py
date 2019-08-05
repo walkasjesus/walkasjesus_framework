4b1f8b0d49125285e97ab5ed574ffdde931aec9f
Lambert <verzend.aan@gmail.com>
2019-08-05 19:37:01 +0200

    Small refactoring

--- volto_website/commandments_app/apps.py
+++ volto_website/commandments_app/apps.py
@@ -1,15 +1,10 @@
 from django.apps import AppConfig
-import vinaigrette
+
+from commandments_app.translation import register_translations
 
 
 class CommandmentsAppConfig(AppConfig):
     name = 'commandments_app'
 
     def ready(self):
-        print('AppReady')
-        # Is this required by vinaigrette?
-        from commandments_app.models import Commandment
-        comm = self.get_model("Commandment")
-
-        # Register fields to translate
-        vinaigrette.register(comm, ['title', 'description'])
+        register_translations(self)
--- volto_website/commandments_app/translation.py
+++ volto_website/commandments_app/translation.py
@@ -1,3 +1,8 @@
-from django.apps import AppConfig
+import vinaigrette
 
 
+def register_translations(app_config):
+    comm = app_config.get_model("Commandment")
+
+    # Register fields to translate
+    vinaigrette.register(comm, ['title', 'description'])

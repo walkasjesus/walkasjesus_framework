5290acf9011a3b0ce5c5c5d7c711ba68436633bc
Lambert <verzend.aan@gmail.com>
2019-08-06 07:10:51 +0200

    Restored arguments in translate shell script, I thought it was causing another issue with rosetta, but that was caused by using django-admin instead of python manage.py

--- volto_website/update_translation_files.sh
+++ volto_website/update_translation_files.sh
@@ -6,7 +6,7 @@ else
 	echo "error: cannot find environment binary"
 fi
 echo "Adding new texts to the .po files."
-python manage.py makemessages
+python manage.py makemessages -l nl
 echo "Translating files."
 python auto_translate_files.py
 echo "Compiling the .po files. Run this also after changing the .po files."

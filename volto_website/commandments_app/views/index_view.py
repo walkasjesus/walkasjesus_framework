957e9151eaba85b7911ee065830af74c8a23fefe
Merijn <info@jesuscommandments.org>
2019-08-15 10:02:32 +0200

    Fixed count commandments and count languages

--- volto_website/commandments_app/views/index_view.py
+++ volto_website/commandments_app/views/index_view.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.views import View
 
 from commandments_app.models import Commandment
-
+from django.conf import settings
 
 class IndexView(View):
     def get(self, request):
+        commandments_all = Commandment.objects.order_by('title')
+        commandments_total = len(commandments_all)
+        languages_all = settings.LANGUAGES
+        languages_total = len(languages_all)
         commandments = Commandment.objects.order_by('title')[0:20]
-        return render(request, 'commandments/index.html', {'commandments': commandments})
+
+        return render(request, 'commandments/index.html', {'commandments': commandments,
+                                                           'languages_all': languages_all,
+                                                           'languages_total': languages_total,
+                                                           'commandments_total': commandments_total})

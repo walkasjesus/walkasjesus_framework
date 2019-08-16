6ee8b23ab1c3a36e47e47f762f51e0a9c829bdfe
Lambert <verzend.aan@gmail.com>
2019-08-16 07:28:26 +0200

    WIP: adding factories and formatters.

--- bible_lib/bible_lib/bible_factory.py
+++ bible_lib/bible_lib/bible_factory.py
@@ -3,9 +3,17 @@ from bible_lib.api_bible import ApiBible
 from bible_lib.hsv_bible import HsvBible
 
 
+class PlainTextFormatter(object):
+    pass
+
+
 class BibleFactory:
-    def create(self, bible_id: str) -> Bible:
+    def __init__(self, text_formatter=PlainTextFormatter()):
+        self.text_formatter = text_formatter
+
+    def create(self, bible_id: str, ) -> Bible:
         if bible_id.lower() == 'hsv':
-            return HsvBible()
+            return HsvBible(self.text_formatter)
         else:
-            return ApiBible(bible_id=bible_id)
+            return ApiBible(bible_id=bible_id, self.text_formatter)
+

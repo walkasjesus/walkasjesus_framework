b440966b1be5b20a82f27a1e9c440a90cf74ba2c
Lambert <verzend.aan@gmail.com>
2019-08-22 20:27:09 +0200

    Deleted unused files.

--- volto_website/commandments_app/urls.py
+++ volto_website/commandments_app/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 
 from commandments_app.views.bible_view import BibleView
-from commandments_app.views.demo_view import DemoView
 from commandments_app.views.detail_view import DetailView
 from commandments_app.views.index_view import IndexView
 from commandments_app.views.listing_view import ListingView
 from commandments_app.views.study_listing_view import StudyListingView
-
 
 app_name = 'commandments'
 
@@ -16,7 +14,6 @@ cache_time_s = 60 * 60
 
 urlpatterns = [
     path('', cache_page(cache_time_s)(IndexView.as_view()), name='index'),
-    path('demo', DemoView.as_view(), name='demo'),
     path('bible', BibleView.as_view(), name='bible'),
     path('listing', cache_page(cache_time_s)(ListingView.as_view()), name='listing'),
     path('study_listing/<str:bible_id>', cache_page(cache_time_s)(StudyListingView.as_view()), name='study_listing'),

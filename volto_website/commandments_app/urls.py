4ae9fa56dda38e83543d69551d419a982f1097cf
Lambert <verzend.aan@gmail.com>
2019-08-23 09:26:06 +0200

    Working on more finegrained caching

--- volto_website/commandments_app/urls.py
+++ volto_website/commandments_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from commandments_app.views.bible_view import BibleView
 from commandments_app.views.detail_view import DetailView
@@ -9,13 +8,10 @@ from commandments_app.views.study_listing_view import StudyListingView
 
 app_name = 'commandments'
 
-
-cache_time_s = 60 * 60
-
 urlpatterns = [
-    path('', cache_page(cache_time_s)(IndexView.as_view()), name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('bible', BibleView.as_view(), name='bible'),
-    path('listing', cache_page(cache_time_s)(ListingView.as_view()), name='listing'),
-    path('study_listing/<str:bible_id>', cache_page(cache_time_s)(StudyListingView.as_view()), name='study_listing'),
-    path('detail/<str:bible_id>/<int:commandment_id>', cache_page(cache_time_s)(DetailView.as_view()), name='detail'),
+    path('listing', ListingView.as_view(), name='listing'),
+    path('study_listing/<str:bible_id>', StudyListingView.as_view(), name='study_listing'),
+    path('detail/<str:bible_id>/<int:commandment_id>', DetailView.as_view(), name='detail'),
 ]
--- volto_website/commandments_app/views/study_listing_view.py
+++ volto_website/commandments_app/views/study_listing_view.py
@@ -1,4 +1,7 @@
+from functools import lru_cache
+
 from bible_lib import BibleFactory
+from bible_lib.performance_time_decorator import performance_time
 from django.shortcuts import render
 from django.views import View
 
@@ -7,16 +10,20 @@ from commandments_app.models import Commandment
 
 class StudyListingView(View):
     def get(self, request, bible_id: str):
+        data_context = self._get_data_context(bible_id)
+        return render(request, 'commandments/study_listing.html', data_context)
+
+    @staticmethod
+    @performance_time
+    @lru_cache(maxsize=None)
+    def _get_data_context(bible_id):
         commandments = list(Commandment.objects.all())
         # TODO this will fail if no prim bible ref! Maybe should be one on one relation in model?
         commandments.sort(key=lambda x: x.primary_bible_references()[0])
-
         primary_bible_references = []
         secondary_bible_references = []
         tertiary_bible_references = []
-
         user_bible = BibleFactory().create(bible_id)
-
         for commandment in commandments:
             commandment.bible = user_bible
 
@@ -35,6 +42,6 @@ class StudyListingView(View):
                          'bible_reference': bible_reference}
                 tertiary_bible_references.append(entry)
 
-        return render(request, 'commandments/study_listing.html', {'primary_bible_references': primary_bible_references,
-                                                                   'secondary_bible_references': secondary_bible_references,
-                                                                   'tertiary_bible_references': tertiary_bible_references})
+        return {'primary_bible_references': primary_bible_references,
+                'secondary_bible_references': secondary_bible_references,
+                'tertiary_bible_references': tertiary_bible_references}

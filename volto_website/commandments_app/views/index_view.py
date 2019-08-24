999d37c09d6aa1b6b581ca6db7bc4479d9d490e3
Merijn <info@jesuscommandments.org>
2019-08-24 20:12:44 +0200

    Updated home page to only display commandments with drawings

--- volto_website/commandments_app/views/index_view.py
+++ volto_website/commandments_app/views/index_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+from bible_lib import bibles
 from commandments_app.models import Commandment
 from django.conf import settings
 
@@ -9,7 +10,7 @@ class IndexView(View):
     def get(self, request):
         languages_all = settings.LANGUAGES
         languages_total = len(languages_all)
-        commandments = Commandment.objects.order_by('title')[0:20]
+        commandments = Commandment.objects.order_by('title')[0:100]
         commandments_total = Commandment.objects.count()
 
         return render(request, 'commandments/index.html', {'commandments': commandments,

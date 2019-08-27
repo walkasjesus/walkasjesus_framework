67145d7daab45b67b31112a425a91a41cde3fb61
Lambert <verzend.aan@gmail.com>
2019-08-27 18:17:00 +0200

    Added bible count (in available languages) to homepage

--- volto_website/commandments_app/views/index_view.py
+++ volto_website/commandments_app/views/index_view.py
@@ -1,5 +1,7 @@
+from bible_lib import Bibles
 from django.conf import settings
 from django.shortcuts import render
+from django.utils.functional import cached_property
 from django.views import View
 
 from django.contrib.auth.models import User
@@ -11,13 +13,26 @@ class IndexView(View):
         languages_total = len(settings.LANGUAGES)
         commandments_total = Commandment.objects.count()
         users_total = User.objects.count()
+        bibles_total = self._bibles_total
 
         commandments_with_background_drawing = [c for c in Commandment.objects.all() if c.background_drawing()]
 
         return render(request, 'commandments/index.html', {'commandments': commandments_with_background_drawing,
                                                            'languages_total': languages_total,
+                                                           'bibles_total': bibles_total,
                                                            'commandments_total': commandments_total,
                                                            'users_total': users_total})
 
+    @cached_property
+    def _bibles_total(self):
+        count = 0
+        bibles = Bibles().list()
+        languages = [code for code, name in settings.LANGUAGES]
+
+        for bible in bibles:
+            if bible.language in languages:
+                count += 1
+
+        return count
 
 

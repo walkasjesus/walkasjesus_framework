11aa4a1eb63f92221ddb3ed6eaebd7761cea6204
Lambert <verzend.aan@gmail.com>
2019-08-30 13:23:12 +0200

    #12: Added admin formatting and urls to bible cache page.

--- volto_website/commandments_app/urls.py
+++ volto_website/commandments_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from commandments_app.views.admin_bible_view import AdminBibleView
-from commandments_app.views.admin_cache_bible import AdminCacheBible
-from commandments_app.views.admin_reset_bibles import AdminResetBibles
+from commandments_app.views.admin.admin_bible_view import AdminBibleView
+from commandments_app.views.admin.admin_cache_bible import AdminCacheBible
+from commandments_app.views.admin.admin_reset_bibles import AdminResetBibles
 from commandments_app.views.bible_view import BibleView
 from commandments_app.views.detail_view import DetailView
 from commandments_app.views.index_view import IndexView
@@ -18,8 +18,7 @@ urlpatterns = [
     path('study_listing', StudyListingView.as_view(), name='study_listing'),
     path('detail/<int:commandment_id>', DetailView.as_view(), name='detail'),
 
-    # Todo rename urls
-    path('admin_bible', AdminBibleView.as_view(), name='admin_bible'),
-    path('admin_reset_bibles', AdminResetBibles.as_view(), name='admin_reset_bibles'),
-    path('admin_cache_bible', AdminCacheBible.as_view(), name='admin_cache_bible'),
+    path('admin/bible', AdminBibleView.as_view(), name='admin_bible'),
+    path('admin/reset_bibles', AdminResetBibles.as_view(), name='admin_reset_bibles'),
+    path('admin/cache_bible', AdminCacheBible.as_view(), name='admin_cache_bible'),
 ]

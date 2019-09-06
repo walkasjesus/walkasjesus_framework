937e42825ff9261daa23c347e2de53ee5c3189aa
Merijn <info@jesuscommandments.org>
2019-09-06 23:55:00 +0200

    Added Vision page and menu item

--- jesus_commandments_website/commandments_app/urls.py
+++ jesus_commandments_website/commandments_app/urls.py
@@ -7,6 +7,7 @@ from commandments_app.views.detail_view import DetailView
 from commandments_app.views.index_view import IndexView
 from commandments_app.views.listing_view import ListingView
 from commandments_app.views.study_listing_view import StudyListingView
+from commandments_app.views.vision_view import VisionView
 
 app_name = 'commandments'
 
@@ -15,6 +16,7 @@ urlpatterns = [
     path('bible', BibleView.as_view(), name='bible'),
     path('listing', ListingView.as_view(), name='listing'),
     path('study_listing', StudyListingView.as_view(), name='study_listing'),
+    path('vision', VisionView.as_view(), name='vision'),
     path('detail/<int:commandment_id>', DetailView.as_view(), name='detail'),
 
     path('admin/reset_bibles', AdminResetBibles.as_view(), name='admin_reset_bibles'),

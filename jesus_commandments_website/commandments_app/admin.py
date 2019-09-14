6a1186d228f1a629ed1c7057a10d326997e00d0f
Merijn <info@jesuscommandments.org>
2019-09-14 09:44:29 +0200

    Removed deprecated adminSite

--- jesus_commandments_website/commandments_app/admin.py
+++ jesus_commandments_website/commandments_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.admin import AdminSite
 
 from commandments_app.models import *
 from commandments_app.views.admin.admin_bible_view import AdminBibleView

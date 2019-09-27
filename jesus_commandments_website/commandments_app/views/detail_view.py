1d50680a5bd6349e3a7f357dfbf134e8c69be89d
Lambert <verzend.aan@gmail.com>
2019-09-27 08:21:21 +0200

    #46: Removed a line too much.

--- jesus_commandments_website/commandments_app/views/detail_view.py
+++ jesus_commandments_website/commandments_app/views/detail_view.py
@@ -8,6 +8,7 @@ class DetailView(View):
     def get(self, request, commandment_id: int):
         commandment = get_object_or_404(Commandment, pk=commandment_id)
         selected_bible = UserPreferences(request.session).bible
+        commandment.bible = selected_bible
         commandment.languages = UserPreferences(request.session).languages
         return render(request, 'commandments/detail.html', {'commandment': commandment,
                                                             'bible': selected_bible})

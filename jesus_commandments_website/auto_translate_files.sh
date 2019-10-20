7872970d77a632840229640e2d466c351410b0ce
Merijn <info@jesuscommandments.org>
2019-10-20 22:19:54 +0200

    Added some explanation to auto_translate_files.sh

--- jesus_commandments_website/auto_translate_files.sh
+++ jesus_commandments_website/auto_translate_files.sh
@@ -10,3 +10,11 @@ echo "Translating files."
 python manage.py auto_translate
 echo "Compiling the .po files. Run this also after changing the .po files."
 django-admin compilemessages
+echo "
+# When running auto_translate_files.sh Google will translate all django variables in a wrong format:
+# By Example:
+# msgid %(model_name)s with this %(field_label)s already exists.
+# msgstr % (Model_name) s mit diesem% (FIELD_LABEL) s ist bereits vorhanden.
+"
+echo ""
+echo "To fix most of the vars run this script: auto_correct_django_locale_vars.sh"

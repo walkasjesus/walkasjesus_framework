8f877a6addb8e79cddcefba69c4edf2ba9ea8002
Merijn <info@jesuscommandments.org>
2019-11-03 20:35:29 +0100

    Restyled form

--- jesus_commandments_website/account_app/forms.py
+++ jesus_commandments_website/account_app/forms.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    username = forms.CharField(max_length=150, required=True, widget=forms.TextInput(attrs={'placeholder': 'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only'}))
+    first_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'placeholder': 'Optional'}))
+    last_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'placeholder': 'Optional'}))
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(attrs={'placeholder': 'Required. Inform a valid email address.'}))
+    password1 = forms.CharField(label=("Password"), widget=forms.PasswordInput(attrs={'placeholder': "Password must contain at least 8 characters and cant be entirely numeric."}))
+    password2 = forms.CharField(label=("Password confirmation"), widget=forms.PasswordInput(attrs={'placeholder': "Enter the same password as above, for verification."}))
+
     agree_toc = forms.BooleanField(required=True, label='I agree with the Terms and Conditions')
 
     class Meta:

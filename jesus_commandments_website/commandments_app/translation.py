f8240daee3d8ab4f67f5cd9a5a891f6661bbc3b1
Merijn <info@jesuscommandments.org>
2020-01-11 11:23:41 +0100

    Removed translation for resource descriptions, since they are most of the time in their own language, it is not necessary to translate that

--- jesus_commandments_website/commandments_app/translation.py
+++ jesus_commandments_website/commandments_app/translation.py
@@ -5,14 +5,7 @@ def register_translations(app_config):
     translatable_model_fields = {
         'Commandment': ['title', 'title_negative', 'devotional'],
         'Drawing': ['description'],
-        'Song': ['description'],
-        'Movie': ['description'],
-        'ShortMovie': ['description'],
-        'Sermon': ['description'],
         'Picture': ['description'],
-        'Testimony': ['description'],
-        'Blog': ['description'],
-        'Book': ['description'],
         'Question': ['text'],
      }
 

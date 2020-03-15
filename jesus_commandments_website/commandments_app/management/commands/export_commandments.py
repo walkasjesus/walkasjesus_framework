60f1b5b862b0ee0609ed07e9e49824e04c072322
Lambert <verzend.aan@gmail.com>
2020-03-15 14:30:23 +0100

    #103: Made an export for commandments, but need to test and make output a argument instead of hardcoded.

--- jesus_commandments_website/commandments_app/management/commands/export_commandments.py
+++ jesus_commandments_website/commandments_app/management/commands/export_commandments.py
@@ -1,5 +1,4 @@
 from django.core.management import BaseCommand
-from django.utils.translation import activate
 from pandas import DataFrame
 
 from commandments_app.models import *
@@ -7,25 +6,27 @@ from commandments_app.models import *
 
 class Command(BaseCommand):
     """ Export the commandments in the database to a csv file. """
+
     def __init__(self):
         columns = ['step',
-                   'category',
-                   'bible_trans_nl_trans_text',
-                   'Principles',
-                   'title_nl',
-                   'title_en',
-                   'title_ot_nl',
-                   'title_ot_en',
-                   'title_negative_en',
-                   'quote',
-                   'quote_source',
-                   'extra',
                    'bible_ref',
                    'bible_ref_positive_negative',
-                   'bible_ref_literal_figurative',
                    'bible_ref_origin',
                    'bible_ref_author',
                    'bible_ref_type',
+                   'category',
+                   'title_nl',
+                   'title_ot_nl',
+                   'title_ot_en',
+                   'title_ot_code',
+                   'title_en',
+                   'title_negative_en',
+                   'devotional_en',
+                   'devotional_nl',
+                   'devotional_source',
+                   'questions',
+                   'quote',
+                   'quote_source',
                    ]
 
         self.data_frame = DataFrame(columns=columns)
@@ -37,35 +38,40 @@ class Command(BaseCommand):
         for item in commandments:
             self.export_commandment(item)
 
-    def export_commandment(self, obj: Commandment):
-        activate('en')
-        self.data_frame.at[self.last_row_index, 'step'] = obj.id
-        self.data_frame.at[self.last_row_index, 'category'] = obj.category
-        self.data_frame.at[self.last_row_index, 'bible_trans_nl_trans_text'] = ''
-        self.data_frame.at[self.last_row_index, 'title_en'] = obj.title
-        self.data_frame.at[self.last_row_index, 'title_ot_en'] = ''
-        self.data_frame.at[self.last_row_index, 'title_negative_en'] = obj.title_negative
-        self.data_frame.at[self.last_row_index, 'quote'] = obj.quote
-        self.data_frame.at[self.last_row_index, 'quote_source'] = obj.quote_source
-        # self.data_frame.at[self.last_row_index, 'extra'] = ''
-        # self.data_frame.at[self.last_row_index, 'Principles'] = ''
+        self.data_frame.to_csv('temp.csv', index=False)
 
-        self.last_row_index += 1
+    def export_commandment(self, obj: Commandment):
+        self.export_main_content(obj)
 
         self.export_bible_reference(obj.primary_bible_reference(), 'primary')
+        [self.export_questions(item) for item in obj.questions()]
         [self.export_bible_reference(item, 'direct') for item in obj.direct_bible_references()]
         [self.export_bible_reference(item, 'indirect') for item in obj.indirect_bible_references()]
         [self.export_bible_reference(item, 'duplicate') for item in obj.duplicate_bible_references()]
         [self.export_bible_reference(item, 'example') for item in obj.example_bible_references()]
         [self.export_bible_reference(item, 'study') for item in obj.study_bible_references()]
 
-    def export_bible_reference(self, bible_ref, type) -> [str]:
+    def export_main_content(self, obj):
+        self.data_frame.at[self.last_row_index, 'step'] = obj.id
+        self.data_frame.at[self.last_row_index, 'category'] = obj.category
+        self.data_frame.at[self.last_row_index, 'title_en'] = obj.title
+        self.data_frame.at[self.last_row_index, 'title_negative_en'] = obj.title_negative
+        self.data_frame.at[self.last_row_index, 'quote'] = obj.quote
+        self.data_frame.at[self.last_row_index, 'quote_source'] = obj.quote_source
+        self.data_frame.at[self.last_row_index, 'devotional_en'] = obj.devotional
+        self.data_frame.at[self.last_row_index, 'devotional_source'] = obj.devotional_source
+        self.last_row_index += 1
+
+    def export_bible_reference(self, bible_ref, type):
         self.data_frame.at[self.last_row_index, 'step'] = bible_ref.commandment.id
         self.data_frame.at[self.last_row_index, 'bible_ref'] = str(bible_ref)
         self.data_frame.at[self.last_row_index, 'bible_ref_positive_negative'] = bible_ref.positive_negative
-        # self.data_frame.at[self.last_row_index, 'bible_ref_literal_figurative'] = bible_ref.
         self.data_frame.at[self.last_row_index, 'bible_ref_origin'] = bible_ref.origin
         self.data_frame.at[self.last_row_index, 'bible_ref_author'] = bible_ref.author
         self.data_frame.at[self.last_row_index, 'bible_ref_type'] = type
+        self.last_row_index += 1
 
+    def export_questions(self, question):
+        self.data_frame.at[self.last_row_index, 'step'] = question.commandment.id
+        self.data_frame.at[self.last_row_index, 'questions'] = question
         self.last_row_index += 1
